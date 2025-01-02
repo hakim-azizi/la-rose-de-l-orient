3ab28aac34108a9bd24d16458d9dd1a5d7701c4b
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2025-01-02 17:58:44 +0100

    fixed bugs in home page animation

--- php/bas.php
+++ php/bas.php
@@ -44,9 +44,36 @@ Enregistr&eacute; &agrave; la C.N.I.L sous le n&deg; : 1269923.<br>
 <p style="width:100%;text-align:center">Copyright &#169; 2008 - <?php echo date("Y"); ?>.<br>
 &#169; <a href="<?php echo $url_acceuil; ?>">La rose de l&#039;orient</a> - Tous droits r&eacute;serv&eacute;s.<br>
 [Design by : &#169; hakazizi <a href="<?php echo $url_acceuil; ?>">la rose de l&#039;orient</a>]</p>
-<script src="<?php echo $url; ?>js/jquery-min_<?php echo time(); ?>.js"></script>
-<script><?php if($url_reel=='/index.php'){ ?>var dimentionne1=document.getElementById('redimention0').scrollHeight;var dimentionne=dimentionne1+'px';document.getElementById('texte0').style.height=dimentionne;<?php $nb_photo=$nb_photo-1; ?>var num=0;var dimentionner1='';var dimentionner='';
-$(function(){$('#texte'+num).delay(5000).hide('scale',500,function(){$('#image'+num).hide('slide',{direction:'right'},500,function(){num=num+1;if(num><?php echo $nb_photo; ?>){num=1}$('#image'+num).show('slide',{direction:'right'},500,function(){$('#texte'+num).show('puff',500);});});});setInterval(function(){$('#texte'+num).delay(4000).hide('scale',500,function(){$('#image'+num).hide('slide',{direction:'right'},500,function(){num=num+1;if(num><?php echo $nb_photo; ?>){num=1}$('#image'+num).show('slide',{direction:'right'},500,function(){var dimentionner1=document.getElementById('redimention'+num).scrollHeight;var dimentionner=dimentionner1+'px';document.getElementById('texte'+num).style.height=dimentionner;$('#texte'+num).show('puff',500);});});});var dimentionner1=document.getElementById('redimention'+num).scrollHeight;var dimentionner=dimentionner1+'px';document.getElementById('texte'+num).style.height=dimentionner;},10000);});<?php } 
+<script src="<?php echo $url; ?>js/jquery-min<?php /* echo '_'.time(); */ ?>.js"></script>
+<script><?php if($url_reel=='/index.php'){ ?>
+$(function () {
+    let num = 0; // Numéro initial
+    const total = Number(<?php echo $nb_photo; ?>); // Total des photos
+    
+    // Lancement de la première animation
+    $(`#image${num}`).show('slide', { direction: 'right' }, 500, function () {
+        $(`#texte${num}`).show('puff', 500);            
+    });
+
+    // Gestion de l'intervalle d'animation
+    setInterval(function () {
+        $(`#texte${num}`).delay(3000).hide('scale', 500, function () {
+            $(`#image${num}`).hide('slide', { direction: 'right' }, 500, function () {
+
+                if (num >= total) {
+
+                    num = 0; // Retour au premier élément
+                }
+                $(`#image${num}`).show('slide', { direction: 'right' }, 500, function () {
+                    $(`#texte${num}`).show('puff', 500);
+                });
+            });
+        });
+}, 6000);
+    
+});
+
+<?php } 
 if($url_reel=='/article/fiche-article.php'){ ?>
 
 
@@ -64,7 +91,7 @@ if($url_reel=='/article/fiche-article.php'){ ?>
 <?php } ?>
 
 </script></body></html>
-<?php mysqli_query($connectionbd,"INSERT INTO `visite` (`ip`) VALUES ('$ip')");
+<?php /* mysqli_query($connectionbd,"INSERT INTO `visite` (`ip`) VALUES ('$ip')"); */
 $verification_date=@date("W");
 $fraicheur_top_vente=mysqli_fetch_array(mysqli_query($connectionbd,"SELECT date FROM date_topvente"));
 if($verification_date!=$fraicheur_top_vente['date']){
@@ -73,7 +100,7 @@ $i=1;
 $article1=mysqli_query($connectionbd,"SELECT id,grande_image,prix,description,code_produit,cathegorie,type FROM produit WHERE stock>0 ORDER BY RAND() LIMIT 10");
 while($article=mysqli_fetch_array($article1))
 {
-mysqli_query($connectionbd,"UPDATE top_vente SET id='".$article['id']."',grande_image='".$article['grande_image']."',prix='".$article['prix']."',description='".$article['description']."',code_produit='".$article['code_produit']."',cathegorie='".$article['cathegorie']."',type='".$article['type']."' WHERE idvente=$i");
+mysqli_query($connectionbd,"UPDATE top_vente SET id='".$article['id']."',petite_image='".$article['grande_image']."',prix='".$article['prix']."',description='".$article['description']."',code_produit='".$article['code_produit']."',cathegorie='".$article['cathegorie']."',type='".$article['type']."' WHERE idvente=$i");
 $i++;}
 mysqli_query($connectionbd,"UPDATE date_topvente SET date=$verification_date");
 } ?>
